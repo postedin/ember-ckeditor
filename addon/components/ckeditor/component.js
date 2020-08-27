33fb349ea1c650ff8863874cd8573afded82885f
robclancy <robbo.clancy@gmail.com>
2020-08-27 19:25:13 -0400

    refactor: debounce to 300ms

--- addon/components/ckeditor/component.js
+++ addon/components/ckeditor/component.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { later, cancel } from '@ember/runloop';
 
-const DEBOUNCE_MS = 250;
+const DEBOUNCE_MS = 300;
 
 class CKEditorComponent extends Component {
   editor = null;
