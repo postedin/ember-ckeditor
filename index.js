7833b7c530e9311172181d1d05b1a10a9509df9b
robclancy <robbo.clancy@gmail.com>
2021-02-02 01:44:08 -0300

    show addon versio in ember inspector

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 /* eslint-env node */
 
+const writeFile = require('broccoli-file-creator');
+const mergeTrees = require('broccoli-merge-trees');
+const version = require('./package.json').version;
+
 module.exports = {
   name: require('./package').name,
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/ember-ckeditor/register-version.js');
+  },
+
+  treeForVendor() {
+    const content = `Ember.libraries.register('Ember CKEditor', '${version}');`;
+    const registerVersionTree = writeFile('ember-ckeditor/register-version.js', content);
+
+    return mergeTrees([registerVersionTree]);
+  },
 };
