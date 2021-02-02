dd665480e78bd75e56bc300395d2a4e393426fc0
robclancy <robbo.clancy@gmail.com>
2021-02-02 01:21:22 -0300

    linting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,6 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     postcssOptions: {
       compile: {
