d2ba9ea667c5a3540d7324edab9dbd6d0a55312a
robclancy <robbo.clancy@gmail.com>
2021-02-02 02:26:42 -0300

    actually updated to ckeditor 25 and added ckeditor to ember inspector info

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ module.exports = {
   },
 
   treeForVendor() {
-    const content = `Ember.libraries.register('Ember CKEditor', '${version}');`;
+    const content = `
+      Ember.libraries.register('Ember CKEditor', '${version}');
+
+      if (document) {
+        document.addEventListener('DOMContentLoaded', function() {
+          if (CKEDITOR_VERSION) {
+            Ember.libraries.register('CKEditor 5', CKEDITOR_VERSION);
+          }
+        });
+      }
+    `;
     const registerVersionTree = writeFile('ember-ckeditor/register-version.js', content);
 
     return mergeTrees([registerVersionTree]);
